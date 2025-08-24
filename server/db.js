0dbe55449b369fdd8c1b180cc8a2f2f3dda7ff75
Xeetij Dahal <86757471+XeetijDahal@users.noreply.github.com>
2025-08-24 21:13:58 +0545

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,10 +1,8 @@
-const { Pool } = require("pg");  // ✅ make sure to import Pool
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: postgresql://languagestudying_user:JUIQcJXm4owCy5UIppNzIT8B7GNqInC9@dpg-d2lgihje5dus738q2u50-a.oregon-postgres.render.com/languagestudying, // use Render DB URL
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: "postgresql://languagestudying_user:JUIQcJXm4owCy5UIppNzIT8B7GNqInC9@dpg-d2lgihje5dus738q2u50-a.oregon-postgres.render.com/languagestudying", // put in quotes, remove comma
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
