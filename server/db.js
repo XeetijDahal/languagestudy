7278bd83d909f404f3a1b2323cc2601c482d9b40
unknown <xeetij321@gmail.com>
2025-08-25 11:23:20 +0545

    uploaded

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: "postgresql://languagestudying_user:JUIQcJXm4owCy5UIppNzIT8B7GNqInC9@dpg-d2lgihje5dus738q2u50-a.oregon-postgres.render.com/languagestudying", // put in quotes, remove comma
+  connectionString: "postgresql://languagestudying_user:JUIQcJXm4owCy5UIppNzIT8B7GNqInC9@dpg-d2lgihje5dus738q2u50-a/languagestudying", // put in quotes, remove comma
   ssl: { rejectUnauthorized: false }
 });
 
